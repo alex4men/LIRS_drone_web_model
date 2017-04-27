294bad085813583775996321cab916c182c54ac8
alex <xanderfomenko@gmail.com>
2017-04-27 16:05:04 +0300

    Removed another commented code

--- imports/dronepatrol/js/api.js
+++ imports/dronepatrol/js/api.js
@@ -10,12 +10,3 @@ $("#area").contextmenu(function() {
   event.preventDefault();
   area.addStation();
 });
-
-//
-// function test(x, y) {
-//   vertices = []
-//   vertices = d3.selectAll('rect').nodes().map(function (el) {
-//     return [parseInt(d3.select(el).attr('x') + size/2), parseInt(d3.select(el).attr('y') + size/2)];
-//   });
-//   console.log(coordsAreInside([x, y], vertices));
-// }
